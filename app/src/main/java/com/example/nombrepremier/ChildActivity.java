06e5a0df2aed9dfe717087a6a5796d7c4a25f022
CyriusWallenstein <58891389+CyriusWallenstein@users.noreply.github.com>
2023-12-08 20:53:35 +0100

    add child activity

--- app/src/main/java/com/example/nombrepremier/ChildActivity.java
+++ app/src/main/java/com/example/nombrepremier/ChildActivity.java
@@ -1,12 +1,8 @@
 package com.example.nombrepremier;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -15,59 +11,72 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
-public class MainActivity extends AppCompatActivity {
+public class ChildActivity extends AppCompatActivity {
     private EditText initialValue;
     private ArrayAdapter<Integer> primeNumbersAdapter;
     private TextView countPrimeNumber;
 
     private Integer primeNumberFind = 0;
+
+    private ExecutorService executorService = Executors.newSingleThreadExecutor(); // You can adjust the number of threads as needed
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_child);
 
         ListView primeNumbersListView = findViewById(R.id.home_page_list_text_view);
         Button startCalculationButton = findViewById(R.id.home_page_btn_calc);
         countPrimeNumber = findViewById(R.id.home_page_text_view_count_result);
         initialValue = findViewById(R.id.home_page_edit_text);
 
-        // Créer un adaptateur pour les nombres premiers
+        // Create an adapter for prime numbers
         primeNumbersAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         primeNumbersListView.setAdapter(primeNumbersAdapter);
 
-        Button btnchangeView = findViewById(R.id.home_page_btn_change_view);
-        btnchangeView.setOnClickListener(v->{
+        Button changeView = findViewById(R.id.child_page_change_view);
 
-            Intent child = new Intent(MainActivity.this, ChildActivity.class);
-
-            startActivity(child);
-
+        changeView.setOnClickListener(v -> {
+            Intent returnIntent = new Intent();
+            setResult(Activity.RESULT_OK, returnIntent);
+            finish();
         });
-
         startCalculationButton.setOnClickListener(v -> {
-            // Exécutez la tâche AsyncTask
+
+            // Execute the task using ExecutorService
             primeNumberFind = 0;
             countPrimeNumber.setText(primeNumberFind.toString());
-            new CalculatePrimesTask().execute(Long.parseLong(initialValue.getText().toString())); // Remplacez 100 par la valeur souhaitée
+
+            Future<List<Long>> future = executorService.submit(new CalculatePrimesTask(Long.parseLong(initialValue.getText().toString())));
+            try {
+                List<Long> result = future.get();
+                updateUI(result);
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
         });
     }
 
+    private class CalculatePrimesTask implements Callable<List<Long>> {
+        private long maxNumber;
 
-    private class CalculatePrimesTask extends AsyncTask<Long, Long, List<Long>> {
-
-        @Override
-        protected void onPreExecute() {
-            // Vous pouvez effectuer des initialisations ici avant le début de la tâche
-            primeNumbersAdapter.clear();
+        CalculatePrimesTask(long maxNumber) {
+            this.maxNumber = maxNumber;
         }
 
         @Override
-        protected List<Long> doInBackground(Long... params) {
-            long maxNumber = params[0];
+        public List<Long> call() throws Exception {
             boolean[] isPrime = new boolean[(int) (maxNumber + 1)];
             Arrays.fill(isPrime, true);
 
@@ -76,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
                     for (long multiple = number * number; multiple <= maxNumber; multiple += number) {
                         isPrime[(int) multiple] = false;
                     }
-                    publishProgress(number); // Mettre à jour l'interface utilisateur pendant l'exécution
+                    updateProgress(number);
                 }
             }
 
@@ -90,20 +99,22 @@ public class MainActivity extends AppCompatActivity {
 
             return primeNumbers;
         }
+    }
 
-        @Override
-        protected void onProgressUpdate(Long... values) {
-            // Mettez à jour l'interface utilisateur avec le nouveau nombre premier
-            primeNumbersAdapter.add(Integer.parseInt(values[0].toString()));
+    private void updateUI(List<Long> result) {
+        Toast.makeText(ChildActivity.this, "HOALLA2", Toast.LENGTH_LONG).show();
+        countPrimeNumber.setText(primeNumberFind.toString());
+        primeNumbersAdapter.clear();
 
-        }
+    }
 
-        @Override
-        protected void onPostExecute(List<Long> result) {
-            Toast.makeText(MainActivity.this, "HOALLA", Toast.LENGTH_LONG).show();
-            countPrimeNumber.setText(primeNumberFind.toString());
-        }
+    private void updateProgress(long value) {
+        runOnUiThread(() -> primeNumbersAdapter.add((int) value));
+    }
 
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        executorService.shutdown();
     }
 }
